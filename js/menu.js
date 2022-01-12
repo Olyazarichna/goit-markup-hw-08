81c16359bbfb6be617bf179696573448c774f9a5
Olyazarichna <olya.zarichna@gmail.com>
2022-01-12 12:48:30 +0200

    style

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,8 @@
     const menuBtnRef = document.querySelector("[data-menu-button]");
 
     menuBtnRef.addEventListener("click", () => {
+        const expanded = menuBtnRef.getAttribute("arai-expended") === "true" || false;
         menuBtnRef.classList.toggle("is-open");
-    
+        menuBtnRef.setAttribute("aria-expanded", !expanded);
     });
 })();
